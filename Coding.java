83bca0401b54a96b42b48f58c2ac17f6476c0b06
Ben Solomon <bds78@pitt.edu>
2020-09-16 14:46:01 -0400

    Update Coding.java

--- Coding.java
+++ Coding.java
@@ -4,8 +4,9 @@ I wonder what this does
 */
 public class Coding
 {
-	public static void main(String args[])
+	public static void main( String args[] )
 	{
 		System.out.println("How do I actually code in this?");
+		System.out.println("Interesting. It doesn't let you compile stuff but it lets you run .class files");
 	}
 }

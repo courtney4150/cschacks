9701cb96a953b5f05666ea019ba0000aa2a4f6bd
courtney4150 <34966195+courtney4150@users.noreply.github.com>
2020-10-15 18:54:53 -0400

    Start on poll

--- functions.js
+++ functions.js
@@ -1,3 +1,75 @@
-function sayHello() {
+function sayHello()
+{
    alert("Hello World")
+}
+
+
+
+function buildQuiz()
+{
+  const output = [];
+
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+      const answer = [];
+
+      for(letter in currentQuestion.answers) {
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter}
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers> ${answers.join('')}" </div>`
+      );
+    }
+  );
+
+  quizContainer.innerHTML = output.join('');
+}
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+const myQuestions = [
+{
+  question: "q1?",
+  answers: {
+    a: "answera",
+    b: "answerb",
+    c: "answerc"
+  },
+  correctAnswer: "c"
+},
+{
+  question: "q2?",
+  answers: {
+    a: "answera",
+    b: "answerb",
+    c: "answerc"
+  },
+  correctAnswer: "c"
+},
+{
+  question: "q3?",
+  answers: {
+    a: "answera",
+    b: "answerb",
+    c: "answerc"
+  },
+  correctAnswer: "c"
+}
+];
+
+buildQuiz();
+
+function showResults()
+{
+
 }

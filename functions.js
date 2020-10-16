2e03808d1d741cf04ff2ea2e47a0fe426172ed95
courtney4150 <34966195+courtney4150@users.noreply.github.com>
2020-10-15 20:31:49 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,91 +1,93 @@
-function buildQuiz()
-{
-  const output = [];
+(function(){
+  function buildQuiz()
+  {
+    const output = [];
 
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-      const answer = [];
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+        const answer = [];
 
-      for(letter in currentQuestion.answers) {
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter}
-            ${currentQuestion.answers[letter]}
-          </label>`
+        for(letter in currentQuestion.answers) {
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter}
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers> ${answers.join('')}" </div>`
         );
       }
+    );
 
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers> ${answers.join('')}" </div>`
-      );
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults()
+  {
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    let numCorrect = 0;
+
+    myQuestions.forEach((currentQuestion, questionNumber) -> {
+
+      const answerConatiner = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]: checked`;
+      const userAnswer = (answerConatiner.querySelector(selector) || {}).value;
+
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      else{
+        answerContainers[questionNumber].style.color = 'red';
+      }
     }
   );
 
-  quizContainer.innerHTML = output.join('');
-}
-
-function showResults()
-{
-  const answerContainers = quizContainer.querySelectorAll('.answers');
-  let numCorrect = 0;
-
-  myQuestions.forEach((currentQuestion, questionNumber) -> {
-
-    const answerConatiner = answerContainers[questionNumber];
-    const selector = `input[name=question${questionNumber}]: checked`;
-    const userAnswer = (answerConatiner.querySelector(selector) || {}).value;
-
-    if(userAnswer === currentQuestion.correctAnswer){
-      numCorrect++;
-
-      answerContainers[questionNumber].style.color = 'lightgreen';
-    }
-    else{
-      answerContainers[questionNumber].style.color = 'red';
-    }
+  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
   }
-);
 
-resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-}
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-const myQuestions = [
-{
-  question: "q1?",
-  answers: {
-    a: "answera",
-    b: "answerb",
-    c: "answerc"
+  const myQuestions = [
+  {
+    question: "q1?",
+    answers: {
+      a: "answera",
+      b: "answerb",
+      c: "answerc"
+    },
+    correctAnswer: "c"
   },
-  correctAnswer: "c"
-},
-{
-  question: "q2?",
-  answers: {
-    a: "answera",
-    b: "answerb",
-    c: "answerc"
+  {
+    question: "q2?",
+    answers: {
+      a: "answera",
+      b: "answerb",
+      c: "answerc"
+    },
+    correctAnswer: "c"
   },
-  correctAnswer: "c"
-},
-{
-  question: "q3?",
-  answers: {
-    a: "answera",
-    b: "answerb",
-    c: "answerc"
-  },
-  correctAnswer: "c"
-}
-];
+  {
+    question: "q3?",
+    answers: {
+      a: "answera",
+      b: "answerb",
+      c: "answerc"
+    },
+    correctAnswer: "c"
+  }
+  ];
 
-buildQuiz();
-showQuiz();
+  buildQuiz();
+  showQuiz();
 
-submitButton.addeventListener('click', showResults);
+  submitButton.addeventListener('click', showResults);
+})();

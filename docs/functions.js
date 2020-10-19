6ea168ba294a5a82066bf74596fbe8e6ce061c9d
courtney4150 <34966195+courtney4150@users.noreply.github.com>
2020-10-18 20:27:15 -0400

    Update functions.js

--- docs/functions.js
+++ docs/functions.js
@@ -50,10 +50,7 @@ const answers = [];
         answers.push('<label>' '<input type="radio" name="question'+i+'" value="'+letter+'">' + letter + ': ' + questions[i].answers[letter] + '</label>');
       }
 
-      output.push(
-        '<div class="question">' questions[i].question + '</div>' +
-        '<div class="answers>' answers.join('') + '</div>'
-      );
+      output.push('<div class="question">' questions[i].question + '</div>' + '<div class="answers>' answers.join('') + '</div>');
   }
 quizContainer.innerHTML = output.join('');
 }
